849d1094d5b124350b603558cbcd6f732b7e4a01
Tarylin <jiskelbasumatary41@gmail.com>
2021-02-02 21:19:11 +0530

    json

--- sketch.js
+++ sketch.js
@@ -11,7 +11,8 @@ var bird, slingshot;
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+
+   getbackgroundImg(); 
 }
 
 function setup(){
@@ -45,7 +46,9 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg){
     background(backgroundImg);
+    }
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -85,4 +88,17 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+async function getbackgroundImg(){
+var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+var responseJson = await response.json();
+var time = responseJson.datetime.slice(11,13)
+
+if(time>06&&time<18){
+bg = "sprites/bg.png"
+}
+else{
+  bg = "sprites/bg2.jpg"  
+}
+backgroundImg = loadImage(bg);
 }
